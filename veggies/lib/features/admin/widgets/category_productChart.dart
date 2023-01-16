acd6b873035493661beb932aa6c3a248a4ca00a4
Gideon250 <treygedoxsenior@gmail.com>
2023-01-16 06:44:07 -0800

    changes

--- veggies/lib/features/admin/widgets/category_productChart.dart
+++ veggies/lib/features/admin/widgets/category_productChart.dart
@@ -1,1 +1,18 @@
 
+import 'package:flutter/material.dart';
+import 'package:veggies/features/admin/models/sales.dart';
+
+// class CategoryProductsChart extends StatelessWidget {
+//   final List<charts.Series<Sales, String>> seriesList;
+//   const CategoryProductsChart({
+//     Key? key,
+//     required this.seriesList,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return charts.BarChart(
+//       seriesList,
+//       animate: true,
+//     );
+//   }
